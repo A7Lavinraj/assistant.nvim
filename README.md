92f5e3b1f338be06696d95903196d81dfe1af00b
lavinraj <lavinrajmohan@gmail.com>
2024-04-21 15:29:13 +0530

    docs: Update readme for usage

--- README.md
+++ README.md
@@ -2,20 +2,29 @@
 
 A light-weight competitive programming code tester.
 
-## Installation
-
-[Lazy.nvim](https://github.com/folke/lazy.nvim)
-
-```lua
-{ "A7lavinraj/assistant.nvim" }
-```
-
-## Setup
+## Setup with [Lazy.nvim](https://github.com/folke/lazy.nvim)
 
 ```lua
 {
+  "A7lavinraj/assistant.nvim"
   config = function()
     require("assistant").setup()
   end
 }
+```
+
+## Usage
+
+```lua
+-- Commands to interact with plugin.
+vim.cmd("AssistantRecieve")
+vim.cmd("AssistantRuntest")
+vim.cmd("AssistantToggle")
+
+-- Mapping to interact with plugin.
+local opts = { silent = true, noremap = true }
+
+vim.keymap.set("n", "<leader>af", "<cmd>AssistantRecieve<cr>", opts)
+vim.keymap.set("n", "<leader>at", "<cmd>AssistantRuntest<cr>", opts)
+vim.keymap.set("n", "<leader>ao", "<cmd>AssistantToggle<cr>", opts)
 ```
--- lua/assistant/init.lua
+++ lua/assistant/init.lua
@@ -2,25 +2,18 @@ local receiver = require("assistant.receiver")
 local runner = require("assistant.runner")
 local ui = require("assistant.ui")
 
-local execute_user_commands = function(arg)
-	if arg.fargs[1] == "Receive" then
-		receiver()
-	elseif arg.fargs[1] == "RunTest" then
-		runner()
-	elseif arg.fargs[1] == "Toggle" then
-		ui:toggle()
-	else
-		vim.notify("Invalid Argument", vim.log.levels.ERROR)
-	end
-end
-
 return {
 	setup = function()
-		vim.api.nvim_create_user_command("Assistant", execute_user_commands, {
-			nargs = 1,
-			complete = function()
-				return { "Receive", "RunTest", "Toggle" }
-			end,
+		vim.api.nvim_create_user_command("AssistantRecieve", receiver, {
+			nargs = 0,
+		})
+		vim.api.nvim_create_user_command("AssistantRuntest", runner, {
+			nargs = 0,
+		})
+		vim.api.nvim_create_user_command("AssistantToggle", function()
+			ui:toggle()
+		end, {
+			nargs = 0,
 		})
 	end,
 }
