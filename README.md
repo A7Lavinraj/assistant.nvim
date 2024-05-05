fa9d10a84fc19c355e376d65074dcc18372fc5d4
lavinraj <lavinrajmohan@gmail.com>
2024-05-05 15:27:53 +0530

    docs: populating readme with info and demo

--- README.md
+++ README.md
@@ -2,30 +2,71 @@
 
 A light-weight competitive programming code tester.
 
+## Plugin UI
+
+**Home**
+![Home](./screenshots/screenshot-1.png)
+
+**Run Test**
+![Run Test](./screenshots/screenshot-2.png)
+
+## Plugin in action
+
+https://github.com/A7Lavinraj/assistant.nvim/assets/107323410/2f8ec7d7-2035-42a8-b8e7-e81db469facf
+
 ## Setup with [Lazy.nvim](https://github.com/folke/lazy.nvim)
 
 ```lua
+-- Example to setup for C++ and Python
 {
   "A7lavinraj/assistant.nvim",
   config = function()
-    require("assistant").setup()
+    require("assistant").setup({
+	commands = {
+	    python = {
+		extension = "py",
+		compile = nil,
+		execute = { main = "python3", args = { "$FILENAME_WITH_EXTENSION" } },
+	    },
+	    cpp = {
+	    extension = "cpp",
+		compile = { main = "g++", args = { "$FILENAME_WITH_EXTENSION", "-o", "$FILENAME_WITHOUT_EXTENSION" } },
+		execute = { main = "./$FILENAME_WITHOUT_EXTENSION", args = nil },
+	    },
+	  },
+    })
   end
 }
 ```
 
-## Usage
+## Important takeaways
 
-| Command           | description                                              |
-| ----------------- | -------------------------------------------------------- |
-| :AssistantRecieve | Starting tcp server for competitive companion extension. |
-| :AssistantRuntest | Running source code against sample testcases             |
-| :AssistantToggle  | Toggling user interface of Asisstant.nvim                |
+This plugin doesn't show any sources for a problem if they are not setup properly in plugin configuration, look for basic setup mention above. plugin setup function accepts options for configuration:
 
 ```lua
--- Mappings to interact with plugin.
-local opts = { silent = true, noremap = true }
+{
+  commands = {
+    python = { -- filetype (look down in the README.md to know how to get filetype of a file)
+      extension = "py", -- file extension for source file
+      compile = nil, -- nil for non compiled languages
+      execute = { main = "python3", args = { "$FILENAME_WITH_EXTENSION" } }, -- execution command
+    },
+    cpp = {
+      extension = "cpp",
+      compile = { main = "g++", args = { "$FILENAME_WITH_EXTENSION", "-o", "$FILENAME_WITHOUT_EXTENSION" } }, -- table for compiled languages with contains main and args attributes
+      execute = { main = "./$FILENAME_WITHOUT_EXTENSION", args = nil },
+    },
+  },
+}
 
-vim.keymap.set("n", "<leader>af", "<cmd>AssistantRecieve<cr>", opts)
-vim.keymap.set("n", "<leader>at", "<cmd>AssistantRuntest<cr>", opts)
-vim.keymap.set("n", "<leader>ao", "<cmd>AssistantToggle<cr>", opts)
+```
+
+## Command to interact with plugin
+
+`AssistantToggle`: Toggles the plugin UI window
+
+## How to get the filetype of a file in neovim
+
+```vim
+:lua print(vim.bo.filetype)
 ```
--- lua/assistant/runner.lua
+++ lua/assistant/runner.lua
@@ -6,107 +6,107 @@ local AssistantRunner = {}
 AssistantRunner.__index = AssistantRunner
 
 local function run(command, testcase, callback)
-	---@diagnostic disable: undefined-field
-	local stdin = vim.uv.new_pipe()
-	local stdout = vim.uv.new_pipe()
-	local stderr = vim.uv.new_pipe()
-	local tbl = {}
+  ---@diagnostic disable: undefined-field
+  local stdin = vim.uv.new_pipe()
+  local stdout = vim.uv.new_pipe()
+  local stderr = vim.uv.new_pipe()
+  local tbl = {}
 
-	local handle, _ = vim.uv.spawn(
-		command.main,
-		{ args = command.args, stdio = { stdin, stdout, stderr } },
-		function(code, _)
-			if code == 0 then
-				tbl.status = "FINISHED"
-				callback(tbl)
-			end
-		end
-	)
+  local handle, _ = vim.uv.spawn(
+    command.main,
+    { args = command.args, stdio = { stdin, stdout, stderr } },
+    function(code, _)
+      if code == 0 then
+        tbl.status = "FINISHED"
+        callback(tbl)
+      end
+    end
+  )
 
-	tbl.status = "RUNNING"
+  tbl.status = "RUNNING"
 
-	local timer = vim.uv.new_timer()
-	timer:start(config.config.time_limit, 0, function()
-		timer:stop()
-		timer:close()
-		handle:close()
+  local timer = vim.uv.new_timer()
+  timer:start(config.config.time_limit, 0, function()
+    timer:stop()
+    timer:close()
+    handle:close()
 
-		if tbl.status == "RUNNING" then
-			tbl.killed = true
-			callback(tbl)
-		end
-	end)
+    if tbl.status == "RUNNING" then
+      tbl.killed = true
+      callback(tbl)
+    end
+  end)
 
-	vim.uv.read_start(stdout, function(_, data)
-		if data then
-			tbl.stdout = data
-		end
-	end)
+  vim.uv.read_start(stdout, function(_, data)
+    if data then
+      tbl.stdout = data
+    end
+  end)
 
-	vim.uv.read_start(stderr, function(_, data)
-		if data then
-			tbl.stderr = data
-		end
-	end)
+  vim.uv.read_start(stderr, function(_, data)
+    if data then
+      tbl.stderr = data
+    end
+  end)
 
-	vim.uv.write(stdin, testcase.input)
+  vim.uv.write(stdin, testcase.input)
 end
 
 local function compile(command, callback)
-	local _, _ = vim.uv.spawn(command.main, { args = command.args }, callback)
+  local _, _ = vim.uv.spawn(command.main, { args = command.args }, callback)
 end
 
 local function comparator(stdout, expected)
-	local function process_str(str)
-		return str:gsub("\n", " "):gsub("%s+", " "):gsub("^%s", ""):gsub("%s$", "")
-	end
+  local function process_str(str)
+    return str:gsub("\n", " "):gsub("%s+", " "):gsub("^%s", ""):gsub("%s$", "")
+  end
 
-	return process_str(stdout) == process_str(expected)
+  return process_str(stdout) == process_str(expected)
 end
 
 function AssistantRunner:run_all(testcases, command)
-	local text = Text:new()
+  local text = Text:new()
 
-	text:newline()
-	text:append(" COMPILING... ", "DiagnosticVirtualTextINFO")
-	text:render()
-	text:clear_text()
+  text:newline()
+  text:append(" COMPILING... ", "DiagnosticVirtualTextINFO")
+  text:render()
+  text:clear_text()
 
-	compile(command.compile, function(compile_code, _)
-		vim.schedule(function()
-			text:clear_screen()
-			text:newline()
-			text:append(" RUNNING... ", "DiagnosticVirtualTextINFO")
-			text:render()
-			text:clear_text()
-		end)
+  compile(command.compile, function(compile_code, _)
+    vim.schedule(function()
+      text:clear_screen()
+      text:newline()
+      text:append(" RUNNING... ", "DiagnosticVirtualTextINFO")
+      text:render()
+      text:clear_text()
+    end)
 
-		if compile_code == 0 then
-			for index, testcase in ipairs(testcases) do
-				run(command.execute, testcase, function(execution_result)
-					text:newline()
+    if compile_code == 0 then
+      for index, testcase in ipairs(testcases) do
+        run(command.execute, testcase, function(execution_result)
+          text:newline()
 
-					if execution_result.killed then
-						text:append(
-							string.format(" Testcase #%d TIME LIMIT EXCEEDED ", index),
-							"DiagnosticVirtualTextWARN"
-						)
-					else
-						if comparator(execution_result.stdout, testcase.output) then
-							text:append(string.format(" Testcase #%d PASSED ", index), "DiagnosticVirtualTextHINT")
-						else
-							text:append(string.format(" Testcase #%d FAILED ", index), "DiagnosticVirtualTextERROR")
-						end
-					end
+          if execution_result.killed then
+            text:append(
+              string.format(" Testcase #%d TIME LIMIT EXCEEDED ", index),
+              "DiagnosticVirtualTextWARN"
+            )
+          else
+            if comparator(execution_result.stdout, testcase.output) then
+              text:append(string.format(" Testcase #%d PASSED ", index), "DiagnosticVirtualTextHINT")
+            else
+              text:append(string.format(" Testcase #%d FAILED ", index), "DiagnosticVirtualTextERROR")
+            end
+          end
 
-					vim.schedule(function()
-						text:clear_screen()
-						text:render()
-					end)
-				end)
-			end
-		end
-	end)
+          vim.schedule(function()
+            text:clear_screen()
+            text:render()
+          end)
+        end)
+      end
+    end
+  end)
 end
 
 return AssistantRunner
