48f903bd3ee106e47f82c78a6d298663f4e8aa3c
lavinraj <lavinrajmohan@gmail.com>
2024-05-26 00:10:11 +0530

    feat(run-single-test): added functionality to run single testcase

--- lua/assistant/state.lua
+++ lua/assistant/state.lua
@@ -1,9 +1,12 @@
+local utils = require("assistant.ui.utils")
+
 local AssistantState = {}
 
 function AssistantState.new()
   local self = setmetatable({}, { __index = AssistantState })
 
   self.CWD = nil
+  self.tab = 0
   self.FILETYPE = nil
   self.FILENAME_WITHOUT_EXTENSION = nil
   self.FILENAME_WITH_EXTENSION = nil
@@ -11,11 +14,15 @@ function AssistantState.new()
   return self
 end
 
-function AssistantState:sync()
+function AssistantState:init()
   self.CWD = vim.fn.expand("%:p:h")
   self.FILETYPE = vim.bo.filetype
   self.FILENAME_WITHOUT_EXTENSION = vim.fn.expand("%:t:r")
   self.FILENAME_WITH_EXTENSION = vim.fn.expand("%:t")
+
+  if self.FILENAME_WITHOUT_EXTENSION ~= "" then
+    self.test_data = utils.fetch(string.format("%s/.ast/%s", self.CWD, self.FILENAME_WITHOUT_EXTENSION))
+  end
 end
 
 return AssistantState
