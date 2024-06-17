f245af787736a6a5ebbc2bb05085b8566a3450e3
lavinraj <lavinrajmohan@gmail.com>
2024-06-17 12:33:01 +0530

    refactor(rebuild): initial stage

--- lua/assistant/init.lua
+++ lua/assistant/init.lua
@@ -1,68 +1,7 @@
-local config = require("assistant.config")
-
 local M = {}
-local sample_directory = string.format("%s/%s/", vim.fn.expand("%:p:h"), ".ast")
 
-local function create_source(filename)
-  local sources = {}
-
-  for key, _ in pairs(config.default.commands) do
-    table.insert(sources, key)
-  end
-
-  vim.ui.select(sources, { prompt = "Select source" }, function(source)
-    if source then
-      local extension = config.default.commands[source].extension
-      vim.cmd(string.format("edit %s.%s | w", filename, extension))
-    end
-  end)
-end
-
-local function store_problem(chunk)
-  if vim.fn.isdirectory(".ast") == 0 then
-    vim.fn.mkdir(".ast")
-  end
-
-  local data = string.match(chunk, "^.+\r\n(.+)$")
-  local filename = vim.json.decode(data).languages.java.taskClass
-
-  if filename then
-    local problem = io.open(sample_directory .. filename, "w")
-
-    if problem then
-      problem:write(tostring(data))
-      create_source(filename)
-    end
-  end
-end
-
-M.setup = function(opts)
-  config.update(opts or {})
-  config.load()
-
-  ---@diagnostic disable: undefined-field
-  local server = vim.loop.new_tcp()
-
-  server:bind("127.0.0.1", 10043)
-  server:listen(128, function(listening_error)
-    assert(not listening_error, listening_error)
-    local client = vim.loop.new_tcp()
-    server:accept(client)
-    client:read_start(function(read_error, chunk)
-      assert(not read_error, read_error)
-
-      if chunk then
-        vim.schedule(function()
-          store_problem(chunk)
-        end)
-      else
-        client:shutdown()
-        client:close()
-      end
-    end)
-  end)
-
-  vim.loop.run()
+function M.setup()
+  print("Setuped!")
 end
 
 return M
