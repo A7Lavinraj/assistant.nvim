93998e8d867bafbc0e6f7ffca4a205d97a45038f
lavinraj <lavinrajmohan@gmail.com>
2024-06-28 12:40:43 +0530

    feat(feat/ui-update): updated ui

--- lua/assistant/ui/prompt.lua
+++ lua/assistant/ui/prompt.lua
@@ -1,36 +1,43 @@
 local emitter = require("assistant.emitter")
 local store = require("assistant.store")
 
-local M = {}
+---@class AssistantPrompt
+local AssistantPrompt = {}
 
-M.is_open = false
+function AssistantPrompt.new()
+  return setmetatable({ is_open = false }, { __index = AssistantPrompt })
+end
 
-function M.is_buf()
-  if not M.buf then
+function AssistantPrompt:is_buf()
+  if not self.buf then
     return false
   end
 
-  return vim.api.nvim_buf_is_valid(M.buf)
+  return vim.api.nvim_buf_is_valid(self.buf)
 end
 
-function M.is_win()
-  if not M.win then
+function AssistantPrompt:is_win()
+  if not self.win then
     return false
   end
 
-  return vim.api.nvim_win_is_valid(M.win)
+  return vim.api.nvim_win_is_valid(self.win)
 end
 
-function M.open(number, field)
-  if M.is_open or M.is_buf() or M.is_win() then
+function AssistantPrompt:on_key(mode, lhs, rhs)
+  vim.keymap.set(mode, lhs, rhs, { buffer = self.buf })
+end
+
+function AssistantPrompt:open(number, field)
+  if self.is_open or self:is_buf() or self:is_win() then
     return
   end
 
-  M.tc_number = number
-  M.field = field
-  M.is_open = true
-  M.buf = vim.api.nvim_create_buf(false, true)
-  M.win = vim.api.nvim_open_win(M.buf, true, {
+  self.tcnumber = number
+  self.field = field
+  self.is_open = true
+  self.buf = vim.api.nvim_create_buf(false, true)
+  self.win = vim.api.nvim_open_win(self.buf, true, {
     relative = "editor",
     height = 20,
     width = 50,
@@ -38,41 +45,50 @@ function M.open(number, field)
     col = math.floor(vim.o.columns / 2) - 25,
     style = "minimal",
     border = "rounded",
-    -- title = "prompt",
-    -- title_pos = "center",
   })
 
-  vim.api.nvim_buf_set_lines(M.buf, 0, -1, false, vim.split(store.PROBLEM_DATA["tests"][M.tc_number][M.field], "\n"))
+  vim.api.nvim_buf_set_lines(
+    self.buf,
+    0,
+    -1,
+    false,
+    vim.split(store.PROBLEM_DATA["tests"][self.tcnumber][self.field], "\n")
+  )
 
-  vim.keymap.set("n", "q", M.close, { buffer = M.buf })
+  self:on_key("n", "q", function()
+    self:close()
+  end)
+  self:on_key("n", "<esc>", function()
+    self:close()
+  end)
 end
 
-function M.close()
-  if M.is_open then
-    store.PROBLEM_DATA["tests"][M.tc_number][M.field] =
-      table.concat(vim.api.nvim_buf_get_lines(M.buf, 0, -1, false), "\n")
+function AssistantPrompt:close()
+  if self.is_open then
+    store.PROBLEM_DATA["tests"][self.tcnumber][self.field] =
+      table.concat(vim.api.nvim_buf_get_lines(self.buf, 0, -1, false), "\n")
 
-    if M.is_win() then
-      vim.api.nvim_win_close(M.win, true)
-      M.win = nil
+    if self:is_win() then
+      vim.api.nvim_win_close(self.win, true)
+      self.win = nil
     end
 
-    if M.is_buf() then
-      vim.api.nvim_buf_delete(M.buf, { force = true })
-      M.buf = nil
+    if self:is_buf() then
+      vim.api.nvim_buf_delete(self.buf, { force = true })
+      self.buf = nil
     end
 
-    M.is_open = false
+    self.is_open = false
     emitter.emit("AssistantRender")
   end
 end
 
-function M.toggle()
-  if M.is_open then
-    M.close()
+function AssistantPrompt:toggle()
+  if self.is_open then
+    self:close()
   else
-    M.open()
+    self:open()
   end
 end
 
-return M
+return AssistantPrompt.new()
