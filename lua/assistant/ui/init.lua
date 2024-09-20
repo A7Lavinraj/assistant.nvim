a07884f126907521bfd1e15bf32cd786a4cf455b
lavinraj <lavinrajmohan@gmail.com>
2024-09-20 15:28:11 +0530

    feat(ui): merge prompt with main and preview window

--- lua/assistant/ui/init.lua
+++ lua/assistant/ui/init.lua
@@ -36,6 +36,23 @@ M.prev = Window.new({
   },
 })
 
+M.prompt = Window.new({
+  h_ratio = 0.2,
+  w_ratio = 0.2,
+  h_align = "center",
+  v_align = "center",
+  enter = true,
+  access = true,
+  config = {
+    relative = "editor",
+    style = "minimal",
+    border = config.border,
+  },
+  win_opts = {
+    winhighlight = "NormalFloat:AssistantWindow,FloatBorder:AssistantWindowBorder",
+  },
+})
+
 function M.toggle()
   if M.main.is_open and M.prev.is_open then
     M.main:remove()
@@ -43,6 +60,14 @@ function M.toggle()
   else
     M.main:create()
     M.prev:create()
+  end
+end
+
+function M.resize()
+  if M.main.is_open and M.prev.is_open then
+    M.main:resize()
+    M.prev:resize()
+    M.prompt:resize()
   end
 end
 
