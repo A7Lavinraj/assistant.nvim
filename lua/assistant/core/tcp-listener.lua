5344fe80c0731234e905340abb861d60a8b7e831
A7Lavinraj <lavinrajmohan@gmail.com>
2024-11-09 09:34:06 +0530

    feat: add a `TCPListener` class for better server control

---
+++ lua/assistant/core/tcp-listener.lua
@@ -0,0 +1,47 @@
+local fs = require("assistant.core.file-system").new()
+
+---@class TCPListener
+---@field server uv.uv_tcp_t?
+---@field client uv.uv_tcp_t?
+local TCPListener = {}
+
+function TCPListener.new()
+  return setmetatable({ server = nil, client = nil }, { __index = TCPListener })
+end
+
+function TCPListener:start()
+  self.server = vim.uv.new_tcp()
+  self.server:bind("127.0.0.1", 10043)
+  self.server:listen(128, function(s_err)
+    if s_err then
+      return
+    end
+
+    self.client = vim.uv.new_tcp()
+    self.server:accept(self.client)
+    self.client:read_start(function(c_err, chunk)
+      if c_err then
+        self.client:close()
+        return
+      end
+
+      if chunk then
+        vim.schedule(function()
+          fs:save(chunk)
+        end)
+      end
+    end)
+  end)
+end
+
+function TCPListener:stop()
+  if not self.client:is_closing() then
+    self.client:close()
+  end
+
+  if not self.server:is_closing() then
+    self.server:close()
+  end
+end
+
+return TCPListener
