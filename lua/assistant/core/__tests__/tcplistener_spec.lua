d5bb0c87fdefa13bbda7a5492b65e326ee9af23e
A7Lavinraj <lavinrajmohan@gmail.com>
2024-12-08 10:26:47 +0530

    test: add `tcplistener` binding test

---
+++ lua/assistant/core/__tests__/tcplistener_spec.lua
@@ -0,0 +1,25 @@
+local tcp = require("assistant.core.tcplistener")
+
+---@param cmd string
+---@return string?
+local function shell(cmd)
+  local handle = io.popen(cmd)
+
+  if not handle then
+    return nil
+  end
+
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+tcp.init()
+vim.loop.sleep(100)
+local result = shell("netstat -an | grep :10043")
+
+if not result then
+  assert(false, "[SHELL] `netstat` command problem")
+else
+  assert.are_same(result:match("127%.0%.0%.1:(%d+)"), "10043", "Port 10043 is not bound")
+end
