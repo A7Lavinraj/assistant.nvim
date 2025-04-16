2e228c691745d31c26413bce8514598ea306702c
A7Lavinraj <lavinrajmohan@gmail.com>
2025-04-16 19:08:54 +0530

    refactor!: adapt new plugin structure

--- lua/assistant/init.lua
+++ lua/assistant/init.lua
@@ -1,29 +1,9 @@
 local M = {}
 
--- Initializer for given `module_list` which are necessary to load at startup
----@param module_list {name:string,opts:table}[]
-function M.init_all(module_list)
-  for _, module in ipairs(module_list) do
-    require("assistant." .. module.name).init(module.opts)
-  end
-end
-
--- Setup function for loading and create user command
----@param opts Ast.Config
+---@param opts Assistant.Config
 function M.setup(opts)
-  M.init_all({
-    { name = "config", opts = opts },
-    { name = "ui.groups" },
-    { name = "core.tcplistener" },
-  })
-
-  vim.api.nvim_create_user_command("AssistantToggle", function()
-    require("assistant.utils").notify_warn(
-      "Please use `Assistant` command instead of `AssistantToggle`, it is deprecated now"
-    )
-    require("assistant.ui").show()
-  end, {})
-  vim.api.nvim_create_user_command("Assistant", require("assistant.ui").show, {})
+  require('assistant.config').overwrite(opts)
+  require('assistant.core.tcp').bind_server()
 end
 
 return M
--- lua/assistant/utils.lua
+++ lua/assistant/utils.lua
@@ -1,75 +1,43 @@
-local M = {}
+local utils = {}
 
-local DELIMITER = "==== PLEASE DO NOT EDIT THIS LINE ====="
-
----@param msg string
-function M.notify_info(msg)
-  vim.notify(msg, vim.log.levels.INFO, { title = "Assistant.nvim" })
+---@param message string
+function utils.info(message)
+  vim.notify(message, vim.log.levels.INFO, { title = 'Assistant.nvim' })
 end
 
----@param msg string
-function M.notify_warn(msg)
-  vim.notify(msg, vim.log.levels.WARN, { title = "Assistant.nvim" })
+---@param message string
+function utils.warn(message)
+  vim.notify(message, vim.log.levels.WARN, { title = 'Assistant.nvim' })
 end
 
----@param msg string
-function M.notify_err(msg)
-  vim.notify(msg, vim.log.levels.ERROR, { title = "Assistant.nvim" })
+---@param message string
+function utils.error(message)
+  vim.notify(message, vim.log.levels.ERROR, { title = 'Assistant.nvim' })
 end
 
-function M.is_buf(buf)
-  if not buf then
-    return false
-  end
-
-  return vim.api.nvim_buf_is_valid(buf)
-end
-
-function M.is_win(win)
-  if not win then
-    return false
-  end
-
-  return vim.api.nvim_win_is_valid(win)
-end
-
----@param received string
----@return string
-function M.get_stream_data(received)
-  return table.concat(vim.split(string.gsub(received, "\r\n", "\n"), "\n", { plain = true }), "\n")
-end
-
----@param stdout string
----@param expected string
----@return boolean
-function M.compare(stdout, expected)
-  local function process_str(str)
-    return (str or ""):gsub("\n", " "):gsub("%s+", " "):gsub("^%s", ""):gsub("%s$", "")
-  end
-
-  return process_str(stdout) == process_str(expected)
-end
-
----@return number, number
-function M.get_view_port()
-  local vh = vim.o.lines - vim.o.cmdheight
-  local vw = vim.o.columns
-
-  if vim.o.laststatus ~= 0 then
-    vh = vh - 1
-  end
-
-  return vh, vw
+---@param str string
+function utils.to_snake_case(str)
+  return str
+    :gsub('(%s)', '_')
+    :gsub('(%u%l)', '_%1')
+    :gsub('(%l)(%u)', '%1_%2')
+    :gsub('^_', '')
+    :gsub('^%s*(.-)%s*$', '%1')
+    :gsub('%s+', '_')
+    :gsub('(%l)(%u)', '%1_%2')
+    :gsub('(%l)(%d)', '%1_%2')
+    :gsub('(%d)(%l)', '%1_%2')
+    :gsub('(%d)(%u)', '%1_%2')
 end
 
 ---@param str string
 ---@param n number
 ---@return table<string>
-function M.slice_first_n_lines(str, n)
+function utils.slice_first_n_lines(str, n)
   local lines = {}
 
-  for line in str:gmatch("[^\n]*") do
-    if line ~= "" then
+  for line in str:gmatch '[^\n]*' do
+    if line ~= '' then
       table.insert(lines, line)
     end
 
@@ -81,87 +49,4 @@ function M.slice_first_n_lines(str, n)
   return lines
 end
 
----@return number?
-function M.get_current_line_number()
-  return tonumber(vim.api.nvim_get_current_line():match("^%s*.+%s*Testcase #(%d+)"))
-end
-
-function M.next_test()
-  local buf = vim.api.nvim_get_current_buf()
-  local win = vim.api.nvim_get_current_win()
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-  local pos = vim.api.nvim_win_get_cursor(win)
-
-  if lines[pos[1]]:match("^%s*.+%s*Testcase #%d+") then
-    pos[1] = pos[1] + 1
-  end
-
-  for i = pos[1], #lines do
-    if lines[i]:match("^%s*.+%s*Testcase #%d+") then
-      pos = { i, 0 }
-      vim.api.nvim_win_set_cursor(win, pos)
-      return
-    end
-  end
-end
-
-function M.prev_test()
-  local win = vim.api.nvim_get_current_win()
-  local buf = vim.api.nvim_get_current_buf()
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-  local pos = vim.api.nvim_win_get_cursor(win)
-
-  if lines[pos[1]]:match("^%s*.+%s*Testcase #%d+") then
-    pos[1] = pos[1] - 1
-  end
-
-  for i = pos[1], 1, -1 do
-    if lines[i]:match("^%s*.+%s*Testcase #%d+") then
-      pos = { i, 0 }
-      vim.api.nvim_win_set_cursor(win, pos)
-      return
-    end
-  end
-end
-
----@param text Ast.Text
----@param input string
----@param output string
-function M.io_to_text(text, input, output)
-  local split_lines = vim.split(input, "\n")
-
-  for _, line in ipairs(split_lines) do
-    text:append(line, "AstTextP"):nl()
-  end
-
-  text:append(DELIMITER, "AstTextDim"):nl()
-
-  split_lines = vim.split(output, "\n")
-
-  for index, line in ipairs(split_lines) do
-    text:append(line, "AstTextP")
-
-    if index < #split_lines then
-      text:nl()
-    end
-  end
-end
-
----@param lines string
-function M.text_to_io(lines)
-  return lines:match("^%s*(.-)%s*\n" .. DELIMITER .. "\n%s*(.-)%s*$")
-end
-
-function M.to_snake_case(str)
-  local result = str:gsub("(%s)", "_"):gsub("(%u%l)", "_%1"):gsub("(%l)(%u)", "%1_%2"):gsub("^_", "")
-
-  return result
-    :gsub("^%s*(.-)%s*$", "%1")
-    :gsub("%s+", "_")
-    :gsub("(%l)(%u)", "%1_%2")
-    :gsub("(%l)(%d)", "%1_%2")
-    :gsub("(%d)(%l)", "%1_%2")
-    :gsub("(%d)(%u)", "%1_%2")
-end
-
-return M
+return utils
